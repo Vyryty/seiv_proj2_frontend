9db88828dde633d52b8afa7ad7d74a2a1e4f8274
WilsonTrin <wilsontrin45@gmail.com>
2023-09-11 10:35:26 -0500

    Update CourseList.vue

--- src/views/CourseList.vue
+++ src/views/CourseList.vue
@@ -6,7 +6,6 @@ import { useRouter } from "vue-router";
 
 const router = useRouter();
 const courses = ref([]);
-//const user = Utils.getStore("user");
 const message = ref("Edit or Delete Courses");
 
 const editCourse = (course) => {
@@ -17,89 +16,72 @@ const viewCourse = (course) => {
   router.push({ name: "view", params: { id: course.id } });
 };
 
+const addCourse = (course) => {
+  router.push({ name: "add", params: { id: course.id } });
+};
+
 const deleteCourse = (course) => {
   CourseServices.delete(course.id)
     .then(() => {
-      //retrieveTutorials();
+	  location.reload();
     })
     .catch((e) => {
       message.value = e.response.data.message;
     });
 };
 
-const displayCourse = (course) => {
-	CourseServices.getAll;
+const retrieveCourses = () => {
+	CourseServices.getAll()
+		.then((response)=>{
+			courses.value = response.data;
+		})
+		.catch((e) => {
+			message.value = e.response.data.message;
+		})
 };
 
-//const retrieveTutorials = () => {
-//  CourseServices.getAllForUser(user.userId)
-//    .then((response) => {
-//      tutorials.value = response.data;
-//    })
-//    .catch((e) => {
-//      message.value = e.response.data.message;
-//    });
-//};
-
-//retrieveTutorials();
-const title = ref('This is the course listings page');
 onMounted(() => {
-  //courses = CourseServices.getAll();
-  console.log(courses.value.name);
+  retrieveCourses();
 });
 </script>
 
 <template>
-	<p><br></p>
-	<h1>{{ title }}</h1>
-	<p><br></p>
-	<div class="container">
-        <table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Department</th>
-					<th>Hours</th>
-					<th>Level</th>
-                    <th>Course Number</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="course in courses" :key="course.id">
-					<router-link :to="{ name: 'courselisting_db', params: { id: course.id}}">
+	<v-container>
+		<v-toolbar>
+			<v-toolbar-title>Course Listings</v-toolbar-title>
+		</v-toolbar>
+		<p><br></p>
+		<div class="container">
+			<v-btn class="mx-2" color="primary" @click="addCourse">Add course</v-btn>
+			<p><br></p>
+			<v-table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Department</th>
+						<th>Course Number</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="course in courses" :key="course.id">
 						<td>{{course.name}}</td>
 						<td>{{course.dept}}</td>
-						<td>{{course.hours}}</td>
-						<td>{{course.level}}</td>
 						<td>{{course.courseNo}}</td>
-					</router-link>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+						<td><v-icon small class="mx-4" @click="viewCourse(course)">
+							mdi-format-list-bulleted-type</v-icon>
+							<v-icon small class="mx-4" @click="editCourse(course)">
+							mdi-pencil</v-icon>
+							<v-icon small class="mx-4" @click="deleteCourse(course)">
+							mdi-trash-can</v-icon>
+						</td>
+					</tr>
+				</tbody>
+			</v-table>
+		</div>
+	</v-container>
 </template>
 
-<!---<script>
-import axios from 'axios'
-
-export default {
-	data() {
-		return {
-			courses: []
-		}
-	},
-	mounted() {
-		this.get();
-	},
-	method: {
-		get(){
-			axios.get('http://localhost:3013/course-t3/courses/').then(res => {
-				console.log(res)
-			});
-		}
-	}
-}
-</script> -->
 <!-- <template>
   <div>
     <v-container>
